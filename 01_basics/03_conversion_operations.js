fa0d56ca095b8310d91616c089b81c48b79d1ab1
RAHUL YADAV <142112628+RY2429@users.noreply.github.com>
2025-01-21 13:38:01 +0000

    conversion

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,32 @@
+let score = "RY"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let value_in_number = Number(score)
+// console.log(typeof(value_in_number))
+// console.log(value_in_number)
+
+/*
+    "33" => 33
+    "33abc" => NaN
+    true => 1; false => 0
+
+*/
+
+let is_logged_in = 2
+
+let value_in_is_logged_in = Boolean(is_logged_in)
+// console.log(value_in_is_logged_in)
+
+/*
+    0 => false; 1 => true; 2 => true
+    "" => false
+    "RY" => true
+*/
+
+let num = 33
+
+let string_num = String(num)
+console.log(string_num)
+console.log(typeof(string_num))
