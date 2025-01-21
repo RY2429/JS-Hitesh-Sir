132d2d558e78ce723507274ca9d353ed2e289929
RAHUL YADAV <142112628+RY2429@users.noreply.github.com>
2025-01-21 10:08:35 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs not browser
+
+console.log(3+3)
+console.log("RY");
+
+let name = "RY"
+let age = 22
+let is_loggedin = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// bollean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+
+// objects 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
